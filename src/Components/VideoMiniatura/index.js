6b64f54e7816461a6a713087aad4bbaf03f1df46
anderperez15 <31624820+anderperez15@users.noreply.github.com>
2019-03-03 21:52:09 -0400

    remove like from videoMiniatura

--- src/Components/VideoMiniatura/index.js
+++ src/Components/VideoMiniatura/index.js
@@ -9,15 +9,6 @@ class VideoMiniatura extends Component {
     this.state = {
       active:false
     };
-
-    this.like = this.like.bind(this);
-  }
-
-  like(id){
-    if(!this.state.active){
-      axios.get(`/like/${id}`).then(()=>this.props.update(this.props.index,2,this.props.like+1));
-      this.setState({active:true})
-    }    
   }
   render(){
     let style = {
